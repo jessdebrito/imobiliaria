0a8a7b09b1c99b3ad8ab687cce3a86b1b2b19bd3
Jéssica de Brito <debrito.herself@gmail.com>
2025-04-07 00:19:50 -0300

    style: modal

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -16,7 +16,7 @@ const PreviewModal = () => {
 
   return (
     <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
-      <div className="max-h-screen grid items-start w-full grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+      <div className=" grid items-start w-full grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
         <div className="sm:col-span-4 lg:col-span-5">
           <Gallery images={product.images} />
         </div>

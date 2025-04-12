ea2134b84cd6606512c51565f583f24d5e6eb5b6
Jéssica de Brito <debrito.herself@gmail.com>
2025-04-12 17:27:21 -0300

    feat analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ToastProvider />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
